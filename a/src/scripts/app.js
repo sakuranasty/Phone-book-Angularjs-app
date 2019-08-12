3c256ffe0c49f22a0c305e3dd3d132df9bcb3ec4
Anastasiya Pavlova <Anastasiya.Pavlova@emergn.com>
2019-08-12 20:05:41 +0300

    added some json

--- a/src/scripts/app.js
+++ a/src/scripts/app.js
@@ -4,44 +4,24 @@ function randomInteger(min,max){
     return Math.floor(min + Math.random()*(max+1-min));
 };
 
-function generateNumber(){
-    let prefixRange = [300,400];
-    let keyRange = [0,100];
-    let indexRange = [0,100];
-    return `${randomInteger(...prefixRange)}-${randomInteger(...keyRange)}-${randomInteger(...indexRange)}`;
-};
-// function showDetails(){
-//     $http({
-//         method: 'GET',
-//         url: 'https://raw.githubusercontent.com/sakuranasty/detailstemplates/master/'+($index+1)+'.png'
-//     }).then(function(response){
-//         $scope.detailMarkup = response.data
-//     },function(){
-//         $scope.detailMarkup = 'FAILED TO LOAD DETAILS';
-//     });
-// };
-
-// var Response;
 function MyListController($scope,$http,$compile) {
-    var persons = [
-        {'name': 'Julie', 'number': generateNumber()},
-        {'name': 'Peter', 'number': generateNumber()},
-        {'name': 'Stephen', 'number': generateNumber()},
-        {'name': 'Hpsuh', 'number': generateNumber()},
-        {'name': 'Daniele', 'number': generateNumber()},
-        {'name': 'Elizabeth', 'number': generateNumber()},
-        {'name': 'Cristine', 'number': generateNumber()}
-    ];
-    $scope.persons = persons;
+    $scope.persons;
 
+    $http.get('https://api.github.com/users').then(function(resp){
+        $scope.persons = resp.data;
+    },
+    function(){
+        document.querySelector(".phone-table").innerHTML = 'failed to load users list';
+    })
+        
     $scope.somevar = 'some blah blah';
-
+    
     $scope.showDetails = function(person){
-        var index = persons.indexOf(person);
-        $scope.detailsUrl='https://raw.githubusercontent.com/sakuranasty/detailstemplates/master/'+(index+1)+'.html';
+        var login = person.login;
+        $scope.detailsUrl='https://api.github.com/users/'+(login);
         $http({
             method: 'GET',
-            url: 'https://raw.githubusercontent.com/sakuranasty/detailstemplates/master/'+(index+1)+'.html'
+            url: 'https://api.github.com/users/'+(login)
         }).then(function(response){
             var div = angular.element(document.querySelector(".phone-details"));
             div.html(response.data);
@@ -54,22 +34,71 @@ function MyListController($scope,$http,$compile) {
         });
     };
     
-
+    
 };
 
-// angular.module('ContactBook').directive('myHtml', ['$compile', function ($compile) {
-//     return {
-//         restrict: 'A',
-//         replace: false,
-//         terminal: true,
-//         priority: 10000,
-//         link: function (scope, element, attrs) {
-//             var template = attrs['myHtml'];
-//             element[0].innerHTML = scope.response;
-//             $compile(element.contents())(scope);
-            
-//         }
-//     };
-//  }]);
 contactBook.controller('MyListController', ['$scope','$http','$compile', MyListController]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function generateNumber(){
+    let prefixRange = [300,400];
+    let keyRange = [0,100];
+    let indexRange = [0,100];
+    return `${randomInteger(...prefixRange)}-${randomInteger(...keyRange)}-${randomInteger(...indexRange)}`;
+};*/
+
+
+// function showDetails(){
+    //     $http({
+        //         method: 'GET',
+        //         url: 'https://raw.githubusercontent.com/sakuranasty/detailstemplates/master/'+($index+1)+'.png'
+        //     }).then(function(response){
+            //         $scope.detailMarkup = response.data
+            //     },function(){
+                //         $scope.detailMarkup = 'FAILED TO LOAD DETAILS';
+                //     });
+                // };
+                
+// var Response;
+
+
+
+
+
+
+
+    // angular.module('ContactBook').directive('myHtml', ['$compile', function ($compile) {
+    //     return {
+    //         restrict: 'A',
+    //         replace: false,
+    //         terminal: true,
+    //         priority: 10000,
+    //         link: function (scope, element, attrs) {
+    //             var template = attrs['myHtml'];
+    //             element[0].innerHTML = scope.response;
+    //             $compile(element.contents())(scope);
+                
+    //         }
+    //     };
+    //  }]);
 
