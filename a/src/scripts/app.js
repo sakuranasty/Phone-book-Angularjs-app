7e681fe6f612d725f82fcc1efc64ecf0a6b20b16
Anastasiya Pavlova <Anastasiya.Pavlova@emergn.com>
2019-08-20 18:07:35 +0300

    add current chaos to save progress

--- a/src/scripts/app.js
+++ a/src/scripts/app.js
@@ -1,40 +1,14 @@
-let contactBook = angular.module('ContactBook',[]);
+let GHViewer = angular.module('GHViewer',['ngRoute']);
 
-function randomInteger(min,max){
-    return Math.floor(min + Math.random()*(max+1-min));
-};
-
-function MyListController($scope,$http,$compile) {
-    $scope.persons;
-    $scope.userInformation;
-
-    $http.get('https://api.github.com/users').then(function(resp){
-        $scope.persons = resp.data;
-    },
-    function(){
-        document.querySelector(".phone-table").innerHTML = 'failed to load users list';
+GHViewer.config(function($routeProvider){
+    $routeProvider.when('/mainpage',{
+        templateUrl: 'userList.html',
+        controller: ''
     })
-        
-    $scope.somevar = 'some blah blah';
-    
-    $scope.showDetails = function(person){
-        var login = person.login;
-        $scope.detailsUrl='https://api.github.com/users/'+(login);
-        $http({
-            method: 'GET',
-            url: 'https://api.github.com/users/'+(login)
-        }).then(function(response){
-            $scope.userInformation = response.data;
-        },
-        function(){
-            document.querySelector('.warning').innerHTML = 'FAILED TO LOAD DETAILS';
-        });
-    };
-    
-    
-};
 
-contactBook.controller('MyListController', ['$scope','$http','$compile', MyListController]);
+});
+
+
 
 
 
